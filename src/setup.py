1d54cdc43666f3241040784302b0c8be7649c08e
Christian González <christian.gonzalez@nerdocs.at>
2018-06-25 13:14:14 +0200

    add a setup.py

---
+++ src/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='pydifact',
+    version='0.0.1',
+    author='Christian González',
+    author_email='office@nerdocs.at',
+    description='A Python EDI file parser.',
+    # install_requires=[],
+    url='https://github.com/nerdocs/pydifact',
+    packages=['pydifact'],
+    license='LGPLv3+',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License '
+            'v3 or later (LGPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        # 'Topic :: Documentation :: Sphinx',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Software Development :: Libraries',
+    ],
+
+)
