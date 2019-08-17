99a355d2078f807e96538519f54a5a3eae75d93a
Christian González <christian.gonzalez@nerdocs.at>
2019-08-18 01:23:24 +0200

    remove redundant setup() code

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="pydifact",
-    version="0.0.1",
-    author="Christian González",
-    author_email="office@nerdocs.at",
-    description="A Python EDI file parser.",
-    # install_requires=[],
-    url="https://github.com/nerdocs/pydifact",
-    packages=["pydifact", "pydifact.control"],
-    license="MIT",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        # 'Topic :: Documentation :: Sphinx',
-        "Programming Language :: Python",
-        "Topic :: Scientific/Engineering :: Medical Science Apps.",
-        "Topic :: Software Development :: Libraries",
-    ],
-)
+setup()
