cfbee727790dc3a199b205ba17090230676dca97
Christian González <christian.gonzalez@nerdocs.at>
2019-11-08 12:47:46 +0100

    move UNASegment to syntax.common, as it's the same everywhere

--- pydifact/syntax/common.py
+++ pydifact/syntax/common.py
@@ -1,3 +1,16 @@
+from pydifact import Segment, Characters
+
+
+class UNASegment(Segment):
+    """Service String Advice."""
+
+    def __init__(self, characters: Characters or str = None):
+        if not characters:
+            characters = Characters()
+        assert len(str(characters)) == 6
+        super().__init__("UNA", characters)
+
+
 def assert_n(s, length):
     """checks if s is numeric and has a given length."""
     assert int(s)
--- pydifact/syntax/v3.py
+++ pydifact/syntax/v3.py
@@ -1,3 +1,5 @@
 from pydifact.control import Characters
 from .common import assert_a, assert_n, assert_an_max, assert_an
 from ..segments import Segment
+
+
