6aed45844510b1db9abb9d700c12a7a79ccd4097
Christian González <christian.gonzalez@nerdocs.at>
2020-04-29 23:44:58 +0200

    update RTD settings

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "pydifact"
-copyright = "2019, Christian González"
+copyright = "2019-2020, Christian González"
 author = "Christian González"
 
 
@@ -28,7 +28,7 @@ author = "Christian González"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -46,9 +46,23 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 
+html_theme_options = {
+    "canonical_url": "https://pydifact.readthedocs.io/",
+    # "logo_only": False,
+    "display_version": True,
+    "prev_next_buttons_location": "both",
+    # Toc options
+    # "collapse_navigation": True,
+    # "sticky_navigation": True,
+    # "navigation_depth": 4,
+    # "includehidden": True,
+    # "titles_only": False,
+}
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
-master_doc = 'index'
+master_doc = "index"
