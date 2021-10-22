3f02ef1f19ec79608a1da5f6656e46be5521829a
Ron Rademaker <ron@connectholland.nl>
2021-10-22 14:17:00 +0200

    Reproduce issue in test

---
+++ tests/test_message_with_colon_at_end.py
@@ -0,0 +1,25 @@
+#    pydifact - a python edifact library
+#    Copyright (C) 2017-2018  Christian González
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from pydifact.segmentcollection import Interchange
+
+
+# This just tests that there's no EDISyntaxError
+def test_message_with_colon_at_end():
+    Interchange.from_file("tests/data/invoice_with_colon_at_end.edi")
+
+
+if __name__ == "__main__":
+    test_message_with_colon_at_end()
