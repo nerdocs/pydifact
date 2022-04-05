7be24c81928f9bb2106dc2626b461238112d8939
Marcel Johannesmann <marcel.johannesmann@engie.com>
2022-04-05 19:08:07 +0200

    extracted edipy - test works

--- pydifact/edi_energy/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import segmentcollection, energy_collections, energy_formats, errors
-
-from .energy_formats import EDIenergy
-from .energy_segments import EDISegment
--- pydifact/edi_energy/errors.py
+++
@@ -1,2 +0,0 @@
-class MessageError(ValueError):
-    pass
