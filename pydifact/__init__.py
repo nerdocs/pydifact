d655267535c858447c272ef699cfc724305aa640
Christian González <christian.gonzalez@nerdocs.at>
2023-04-22 14:25:05 +0200

    bump version to 0.1.6

--- pydifact/__init__.py
+++ pydifact/__init__.py
@@ -22,7 +22,7 @@
 
 from pydifact import segmentcollection, parser, segments, serializer, token, tokenizer
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from .control.characters import Characters
 from .segmentcollection import SegmentCollection
