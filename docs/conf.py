37883220af0a1e3df455d9f53423cf156e1dd8eb
Antonis Christofides <antonis@antonischristofides.com>
2024-08-06 17:14:58 +0300

    Don't repeat documentation for inherited methods

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ author = "Christian González"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"]
+autodoc_inherit_docstrings = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
